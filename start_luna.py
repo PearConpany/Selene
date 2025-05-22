194737c9418a22644a522b6aff9eeed021fb9d57
PearCompany <92413276+PearConpany@users.noreply.github.com>
2025-05-21 19:54:03 -0600

    Add files via upload

---
+++ start_luna.py
@@ -0,0 +1,3 @@
+import sys, selene          # importa tu punto de entrada
+sys.argv = ["selene.py", "--ide"]   # fuerza el argumento
+selene.main()                        # llama a la función main() ya existente
